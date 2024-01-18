a953305a1b65d9f339503707d7b31d8f4a4372f0
Sahil Fernandes <fernandessahil42@gmail.com>
2024-01-18 23:04:48 +0530

    added login api

--- MyProject/MyProjectApp/urls.py
+++ MyProject/MyProjectApp/urls.py
@@ -4,4 +4,5 @@ from . import views as v
 urlpatterns = [
     path('', v.login_url),
     path('home/', v.home_url),
+    path("login", v.login_api)
 ]
--- MyProject/MyProjectApp/views.py
+++ MyProject/MyProjectApp/views.py
@@ -1,10 +1,42 @@
-import re
+from django.http import JsonResponse
 from django.shortcuts import render, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
 def login_url(request):
     return render(request, 'login.html')
+
+
+@csrf_exempt
+def login_api(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        if username == "sahil" and password == "1234":
+            data = {
+                "username": "sahil",
+                "age": 23,
+                "gender": "Male",
+                "Designation": "Software Developer"
+            }
+            response = {}
+            response["status"] = True
+            response["message"] = "Success"
+            response["data"] = data
+            return JsonResponse(response)
+        else:
+            response = {}
+            response["status"] = False
+            response["message"] = "Invalid username or password"
+            response["data"] = []
+            return JsonResponse(response)
+    response = {}
+    response["status"] = False
+    response["message"] = "Get Request not allowed"
+    response["data"] = []
+    return JsonResponse(response)
 
 
 def home_url(request):
